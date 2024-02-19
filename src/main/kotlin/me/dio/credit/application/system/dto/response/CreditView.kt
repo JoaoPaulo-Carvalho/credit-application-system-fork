997ca5a787ba9381abf8c8ac7bda1dbbbd7f0adf
João Paulo de Carvalho Araújo <araujojoaopaulodecarvalho@gmail.com>
2024-02-19 18:44:07 -0300

    ajuste output saveCredit e Credit views

--- src/main/kotlin/me/dio/credit/application/system/dto/response/CreditView.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/response/CreditView.kt
@@ -3,12 +3,14 @@ package me.dio.credit.application.system.dto.response
 import me.dio.credit.application.system.entity.Credit
 import me.dio.credit.application.system.enummeration.Status
 import java.math.BigDecimal
+import java.time.LocalDate
 import java.util.*
 
 data class CreditView(
   val creditCode: UUID,
   val creditValue: BigDecimal,
   val numberOfInstallment: Int,
+  val dayFirstOfInstallment: LocalDate,
   val status: Status,
   val emailCustomer: String?,
   val incomeCustomer: BigDecimal?
@@ -17,6 +19,7 @@ data class CreditView(
     creditCode = credit.creditCode,
     creditValue = credit.creditValue,
     numberOfInstallment = credit.numberOfInstallments,
+    dayFirstOfInstallment = credit.dayFirstInstallment,
     status = credit.status,
     emailCustomer = credit.customer?.email,
     incomeCustomer = credit.customer?.income
--- src/main/kotlin/me/dio/credit/application/system/dto/response/CreditViewList.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/response/CreditViewList.kt
@@ -2,16 +2,19 @@ package me.dio.credit.application.system.dto.response
 
 import me.dio.credit.application.system.entity.Credit
 import java.math.BigDecimal
+import java.time.LocalDate
 import java.util.*
 
 data class CreditViewList(
   val creditCode: UUID,
   val creditValue: BigDecimal,
-  val numberOfInstallments: Int
+  val numberOfInstallments: Int,
+  val dayFirstOfInstallment: LocalDate
 ) {
   constructor(credit: Credit) : this(
     creditCode = credit.creditCode,
     creditValue = credit.creditValue,
-    numberOfInstallments = credit.numberOfInstallments
+    numberOfInstallments = credit.numberOfInstallments,
+    dayFirstOfInstallment = credit.dayFirstInstallment
   )
 }
